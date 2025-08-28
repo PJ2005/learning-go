a75cd68c139c5f4e37b43635e31633bdcacbbaf5
PJ2005 <pratham.j2905@gmail.com>
2025-08-28 16:55:07 +0530

    added comments

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,12 @@
 package main
 
+// formatting package
 import "fmt"
 
+// external package for quote
 import "rsc.io/quote"
 
+// exected by default when main package is run
 func main() {
     fmt.Println(quote.Go())
 }
