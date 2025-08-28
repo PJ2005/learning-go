9d3743c3c5625ea2d518cf7c7d07be72300985b0
PJ2005 <pratham.j2905@gmail.com>
2025-08-28 17:12:01 +0530

    Added error handling and logging
    If a name is not passed, return an error message and log it into the terminal as well.

--- caller/caller.go
+++ caller/caller.go
@@ -2,11 +2,27 @@ package main
 
 import(
 	"fmt"
+	"log"
+
 	"PJ2005/greetings"
 )
 
 func main(){
-	// get message from greetings module and print
-	message := greetings.Hello("Rohan")
+
+	// command at start of each log message
+	log.SetPrefix("greetings: ")
+
+	// 0 as we are not putting any other fiekds in the output message
+	log.SetFlags(0)
+
+	// get message and error from greetings module
+	message, err := greetings.Hello("")
+
+	// if the error message value is not nil, terminate the code and log the error
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	// else print the message returned
 	fmt.Println(message)
 }
